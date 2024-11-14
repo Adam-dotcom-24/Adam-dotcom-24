83f4d6278b6e564220007fe507f0af0e16657d75
Adam-dotcom-24 <jp.prakash0223@gmail.com>
2024-11-14 11:28:01 +0800

    Delete advertising.py

--- advertising.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-from sklearn.naive_bayes import GaussianNB
-
-st.write("# Simple Advertising Prediction App")
-st.write("This app predicts the Sales type!")
